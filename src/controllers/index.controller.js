4a3e57947e36fe891d7b1f7937802ce20d7576e0
DaniEsp285M <daniel285morales@outlook.com>
2023-11-21 22:15:32 -0600

    version three

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,5 +1,7 @@
 import { pool } from "../db.js";
 
+export const index = (req, res) => res.json({ message: "welcome to my api" });
+
 export const ping = async(req,res)=>{
     const [result] = await pool.query('SELECT * FROM Libros')
     res.json(result)
